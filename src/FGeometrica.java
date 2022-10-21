a6d4457ac21156fe3cd566e9481bb2c0dc5406d7
Daysuke <daysuke.rojas@gmail.com>
2022-10-21 08:49:06 -0500

    Polimorfismo

---
+++ src/FGeometrica.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FGeometrica {
+    protected double ladoA;
+    protected double ladoB;
+    protected double ladoC;
+    protected double area;
+
+    public void pedirDatos(){
+        Scanner RC = new Scanner(System.in);
+
+        System.out.println("Ingrese lado A: ");
+        ladoA =  RC.nextDouble();
+        System.out.println("Ingrese lado B: ");
+        ladoB = RC.nextDouble();
+        System.out.println("Ingrese lado C: ");
+        ladoC = RC.nextDouble();
+    }
+
+    public void mostrarArea(){
+        System.out.println("El area solicitada es: "+area);
+
+    }
+
+}
