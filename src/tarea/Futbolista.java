956321092cd6c3fc4f3d2f6b614d1b5277ec413f
Daysuke <daysuke.rojas@gmail.com>
2022-10-21 11:15:00 -0500

    Polimorfismo

--- src/tarea/Futbolista.java
+++ src/tarea/Futbolista.java
@@ -1,4 +1,34 @@
 package tarea;
 
-public class Futbolista {
+public class Futbolista extends SeleccionFutbol{
+    private int dorsal;
+    private String demarcacion;
+    public Futbolista(){
+        super();}
+    public Futbolista(int id, String nombre, String apellidos, int edad, int dorsal, String demarcacion){
+        super(id, nombre, apellidos, edad);
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;}
+    public int getDorsal(){
+        return dorsal;
+    }
+    public void setDorsal(int dorsal){
+        this.dorsal = dorsal;
+    }
+    public String getDemarcacion(){
+        return demarcacion;
+    }
+    public void setDemarcacion(String demarcacion){
+        this.demarcacion = demarcacion;
+    }
+
+    public void entrenar(){
+        System.out.println("Reliza un entrenamiento (Clase Futbolista)");
+    }
+    public void jugarPartido(){
+        System.out.println("Juega un partido (Clase Futbolista)");
+    }
+    public void entrevista(){
+        System.out.println("Da una entrevista");
+    }
 }
